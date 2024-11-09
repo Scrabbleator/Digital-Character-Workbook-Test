443c097da7f0f289199c0a4db1282c874b23bac6
Scrabbleator <anselmlionel@me.com>
2024-11-09 17:25:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from basic_profile import basic_profile_section
 from diamond_character import diamond_character_section
+from character_evaluation import character_evaluation_section
 
 st.title("Digital Character Workbook Test")
 st.write("This is a basic test to ensure Streamlit is working correctly.")
